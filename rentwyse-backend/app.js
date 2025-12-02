f4421dff91d573fe1bd70515eaa78815f80097cd
sina <sina@DESKTOP-KJM2ALJ>
2025-12-02 16:34:06 -0500

    updated backend github action workflow file

--- rentwyse-backend/app.js
+++ rentwyse-backend/app.js
@@ -55,8 +55,8 @@ const corsOptions = {
     if (!origin) return callback(null, true);
 
     const allowedOrigins = [
-      "http://localhost:4200/",
-      "http://127.0.0.1:4200/",
+      "http://localhost:4200",
+      "http://127.0.0.1:4200",
       "https://rentwise-client-75315342fb4d.herokuapp.com",
       
     ];
